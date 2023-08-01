d590abf477853e3efe7f6d98d48c6089019b55d0
Moustapha76 <moustapha.md26@gmail.com>
2023-08-02 00:34:25 +0200

    pages internes

--- public/js/main.js
+++ public/js/main.js
@@ -10,15 +10,15 @@ itemChild.addEventListener('mouseout', function() {
     itemChild.style.visibility = "hidden"
 })
 
-let playvid = document.querySelector('span.videoPlay');
-let stopvid = document.querySelector('span.videoStop');
-let popup = document.querySelector('.popup');
-playvid.addEventListener('click', function() {
-    popup.style.display = "flex";
-})
-stopvid.addEventListener('click', function() {
-    popup.style.display = "none";
-})
+const playvid = document.querySelector('span.videoPlay');
+const stopvid = document.querySelector('span.videoStop');
+const popup = document.querySelector('.popup');
+// playvid.addEventListener('click', function() {
+//     popup.style.display = "flex";
+// })
+// stopvid.addEventListener('click', function() {
+//     popup.style.display = "none";
+// })
 
 let navbar = document.querySelector('nav.navbar')
 window.addEventListener('scroll', function() {
@@ -28,3 +28,51 @@ window.addEventListener('scroll', function() {
         navbar.classList.remove('fixed')
     }
 })
+// function sendMail(){
+//     var params = {
+//         from_name : document.getElementById('name').value,
+//         tel : document.getElementById('phone').value,
+//         subject : document.getElementById('subject').value,
+//         email_id : document.getElementById('email').value,
+//         message : document.getElementById('message').value,
+//     }
+//     emailjs.sendForm("service_6ldcpf1", "template_xt5f61u ", params).then(function(res){
+//         alert('succes !'+ res.status)
+//     })
+//     alert('error !'+ res.status)
+// }
+const btn = document.getElementById('button');
+btn.addEventListener('click', function() {
+
+  btn.value = 'Sending...';
+  var params = {
+        name : document.getElementById('name').value,
+        phone : document.getElementById('phone').value,
+        subject : document.getElementById('subject').value,
+        email : document.getElementById('email').value,
+        message : document.getElementById('message').value,
+    }
+  const serviceID = 'service_6ldcpf1';
+  const templateID = 'template_xt5f61u';
+
+  emailjs.send(serviceID, templateID, params)
+   .then(() => {
+     btn.value = 'Send Email';
+     alert('Sent!');
+   }, (err) => {
+     btn.value = 'Send Email';
+     alert(JSON.stringify(err));
+   });
+
+// emailjs.send("service_6ldcpf1","template_xt5f61u",{
+//     from_name: "Moussa",
+//     to_name: "moustapha.md26@gmail.com",
+//     name: "Moussa",
+//     phone: "0656716530",
+//     subject: "test subjet",
+//     email: "test@gmail.com",
+//     message: "test msg",
+//     });
+// alert('test')
+});
+
